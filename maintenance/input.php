f3f7e81106cda3f906dea108c844cb56412de851
Ryan F <ryanfaatih.firdaus@gmail.com>
2024-02-06 11:04:50 +0700

    Checkbox void

--- maintenance/input.php
+++ maintenance/input.php
@@ -29,18 +29,32 @@ if (isset($_POST['tanggal'])) {
         if (isset($_POST["numberSN"]) && isset($_POST["inputKerusakan"])) {
             $numberSNArray = $_POST["numberSN"];
             $keteranganArray = $_POST["inputKerusakan"];
+            $voidArray = $_POST["void"];
 
             // Loop through the arrays and insert records
             foreach ($numberSNArray as $key => $no_sn) {
                 $keterangan = $keteranganArray[$key];
+                $void = isset($voidArray[$key]) ? $voidArray[$key] : 0;
 
                 // Insert the new record into detail_maintenance table
                 $queryDetail = "INSERT INTO detail_maintenance (transaksi_id, no_sn, 
                 keterangan, kedatangan, cek_barang, berita_as, administrasi, pengiriman, no_resi) VALUES (?, ?, ?, '1', '0','0','0','0', '0')";
                 $stmtDetail = $conn->prepare($queryDetail);
                 $stmtDetail->bind_param("iis", $transaksi_id, $no_sn, $keterangan);
-                $stmtDetail->execute();
-                $stmtDetail->close();
+                if ($stmtDetail->execute()) {
+                    $stmtDetail->close();
+            
+                    // Update the garansi_void column in inventaris_produk table
+                    $queryUpdateInventaris = "UPDATE inventaris_produk SET garansi_void = ? WHERE no_sn = ?";
+                    $stmtUpdateInventaris = $conn->prepare($queryUpdateInventaris);
+                    $stmtUpdateInventaris->bind_param("ii", $void, $no_sn);
+            
+                    if ($stmtUpdateInventaris->execute()) {
+                        $stmtUpdateInventaris->close();
+                    } else {
+                        echo "Error updating inventaris_produk: " . $stmtUpdateInventaris->error;
+                    }
+                }
             }
         } else {
             echo "Error: Products and other arrays are not set.";
@@ -378,6 +392,7 @@ if (isset($_POST['tanggal'])) {
                                                 <tr>
                                                     <td class="text-center lebar-kolom2" style="min-width:120px;"><b>Nomor SN <span style="color: red;">*</span></b></td>
                                                     <td class="text-center lebar-kolom4" style="min-width:120px;"><b>Kerusakan <span style="color: red;">*</span></b></td>
+                                                    <td class="text-center lebar-kolom5"><b>Void</b></td>
                                                     <td class="text-center lebar-kolom5"><b>Aksi</b></td>
                                                 </tr>
                                             </thead>
@@ -473,6 +488,10 @@ if (isset($_POST['tanggal'])) {
 
                         cols += '<td><input type="number" class="form-control" name="numberSN[]" value="" placeholder="Nomor SN"/></td>';
                         cols += '<td><input type="text" class="form-control" name="inputKerusakan[]" value="" placeholder="Kerusakan Device"/></td>';
+                        cols += '<td>';
+                        cols += '<input class="form-check-input larger-checkbox" type="checkbox" value="1" name="void[]">';
+                        cols += '<input type="hidden" name="void[]" value="0">'; // Hidden input for unchecked checkbox
+                        cols += '</td>';                        
                         cols += '<td><input type="button" class="ibtnDel btn btn-md btn-danger"  value="Delete"></td>';
 
                         newRow.append(cols);
@@ -512,19 +531,15 @@ if (isset($_POST['tanggal'])) {
             var tanggal = document.getElementById("tanggal").value;
 
             // Use classes for dynamic elements
-            var namaElements = document.querySelectorAll(".pilihNamaProduk");
             var numberSNElements = document.querySelectorAll("[name^='numberSN']");
-            var garansiElements = document.querySelectorAll("[name^='pilihGaransi']");
             var keteranganElements = document.querySelectorAll("[name^='inputKerusakan']");
 
             // Check each row
-            for (var i = 0; i < namaElements.length; i++) {
-                var nama = namaElements[i].value;
+            for (var i = 0; i < numberSNElements.length; i++) {
                 var numberSN = numberSNElements[i].value;
-                var garansi = garansiElements[i].value;
                 var keterangan = keteranganElements[i].value;
 
-                if (nama === "" || numberSN === "" || garansi === "" || keterangan === "") {
+                if (numberSN === "" || keterangan === "") {
                     Swal.fire({
                         icon: 'error',
                         title: 'Oops...',
