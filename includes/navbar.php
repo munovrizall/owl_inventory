1db0996053d39e5173d26f64e40641b0abc26d12
Mohammad Novrizal <muhammadnovrizal87@gmail.com>
2024-02-15 11:27:43 +0700

    added username in navbar

--- includes/navbar.php
+++ includes/navbar.php
@@ -1,3 +1,7 @@
+<?php
+$username = $_SESSION['username'];
+?>
+
 <nav class="main-header navbar navbar-expand navbar-dark navbar-dark">
       <!-- Left navbar links -->
       <ul class="navbar-nav">
@@ -8,6 +12,11 @@
 
       <!-- Right navbar links -->
       <ul class="navbar-nav ml-auto">
+        <li class="nav-item">
+          <a class="nav-link">
+            <i class="fas fa-user" style="margin-right: 4px"></i> <?php echo $username?>
+          </a>
+        </li>
         <li class="nav-item">
           <a class="nav-link" href="/owl_inventory/logout.php">
             <i class="fas fa-sign-out-alt"></i> Logout
